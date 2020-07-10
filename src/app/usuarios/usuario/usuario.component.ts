d2d398d01790a1dea7d41be364d8befe8eb563f4
Rabindranath Ferreira <ferreiralox@gmail.com>
2020-07-09 20:22:40 -0400

    add userGetById into user view with store and ngrx

--- src/app/usuarios/usuario/usuario.component.ts
+++ src/app/usuarios/usuario/usuario.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store/app.reducers';
 
 import * as userAction from '../../store/actions/user.actions';
+import { Usuario } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-usuario',
@@ -13,12 +14,14 @@ import * as userAction from '../../store/actions/user.actions';
 export class UsuarioComponent implements OnInit {
 
   id: any;
+  user: Usuario;
 
   constructor( private router: ActivatedRoute,
                private store: Store<AppState>) { }
 
   ngOnInit() {
     this.getUserId();
+    this.loadUser()
   }
 
   /**
@@ -28,6 +31,16 @@ export class UsuarioComponent implements OnInit {
     this.router.params.subscribe( ({id}) => {
       this.id = id ? id : null;
       this.store.dispatch( userAction.LoadUser( { id: this.id } ) );
+    });
+  }
+
+  /**
+   * loadUser
+   */
+  public loadUser() {
+    this.store.select('user').subscribe( ( { user }) => {
+      console.log('user:::', user);
+      this.user = user;
     });
   }
 
